713ee0f2276c4692ac86cef368a35c805610e50e
sunitha <sg397@njit.edu>
2018-10-01 22:40:21 -0400

    Task: Created initial project setup to start mini project to read csv and output html table

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunitha
+ * Date: 10/1/18
+ * Time: 10:11 PM
+ */
+
+echo 'test 124454542';
+$str =' Welcome Sunitha!!';
+echo $str;
---
+++ readme.md
@@ -0,0 +1,7 @@
+#IS-601 Mini Project 
+##This is a sample project to read a csv file and output a HTML table with csv contents
+1. Test
+2. Abc
+3. Good
+
+//Public - Source code

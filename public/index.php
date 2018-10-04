dd5bcaf75a63d79c65ef2ee88d341ca350ad9920
sunitha <sg397@njit.edu>
2018-10-04 17:44:44 -0400

    Feature: Added code to read a csv file records into an array

--- public/index.php
+++ public/index.php
@@ -6,6 +6,35 @@
  * Time: 10:11 PM
  */
 
-echo 'test 124454542';
-$str =' Welcome Sunitha!!';
-echo $str;
+
+main::start("example.csv");
+
+class main {
+    static public function start($filename){
+
+        echo 'Start 123....';
+        $records = csv::getRecords($filename);
+
+        print_r($records);
+
+    }
+}
+
+class csv{
+    public static function getRecords($filename){
+        echo 'in  csv::getRecords';
+        $file = fopen (  $filename, "r");
+
+        while ( ! feof($file)){
+
+            $record = fgetcsv($file);
+            $records[] = $record;
+
+        }
+        fclose($file);
+        return $records;
+
+    }
+}
+
+

41e055a622672e098fd3e73505bb2f6c988a11bb
sunitha <sg397@njit.edu>
2018-10-05 17:35:07 -0400

    Feature: Implemented factory pattern to read csv records as an array of objects

--- public/index.php
+++ public/index.php
@@ -9,32 +9,89 @@
 
 main::start("example.csv");
 
+/**
+ * classname: main
+ * input: filename
+ * output: html table
+ * */
+
 class main {
+
     static public function start($filename){
 
-        echo 'Start 123....';
         $records = csv::getRecords($filename);
 
-        print_r($records);
+        $table = html::generateTable($records);
 
     }
 }
 
+class html{
+    public static function generateTable($records){
+
+        foreach($records as $record){
+            $array = $record->returnArray();
+             print_r($array);
+        }
+    }
+
+}
+
 class csv{
     public static function getRecords($filename){
-        echo 'in  csv::getRecords';
         $file = fopen (  $filename, "r");
-
+        $fieldNames = array();
+        $record = array();
+        $count =0;
         while ( ! feof($file)){
 
             $record = fgetcsv($file);
-            $records[] = $record;
 
+            if($count==0){
+                $fieldNames = $record;
+            } else {
+                $records[] = recordFactory::create($fieldNames, $record);
+            }
+
+
+            $count++;
         }
+
         fclose($file);
         return $records;
 
     }
+}
+
+class record {
+    public function __construct(Array $fieldNames = null,  $values = null)
+    {
+
+        $record = array_combine($fieldNames, $values );
+        foreach ($record as $property => $value){
+            $this->createProperty($property , $value);
+        }
+
+
+    }
+
+    public function returnArray(){
+        $array = (array) $this;
+        return $array;
+    }
+    public function createProperty($name= "NAME", $value="VALUE") {
+        $this->{$name} = $value;
+
+    }
+}
+
+class recordFactory {
+    public static  function create(Array $fieldNames = null,  $values = null) {
+        $record = new record($fieldNames, $values);
+
+        return $record;
+    }
+
 }
 
 
